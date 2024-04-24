c4fd838841f9f158eb5e966998a0d119df4f5fe6
not-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-04-24 17:56:02 -0400

    misc, get it on the cloud

--- lib/components/input.tsx
+++ lib/components/input.tsx
@@ -2,4 +2,7 @@ function ShortTextInput(props: any) {
     const text = props.text;
     const height = props.height;
 
+    return (
+        <input type="text" name={text} placeholder={text} style={{height: height}} />
+    );
 }
