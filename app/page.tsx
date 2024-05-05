b2aff7432a550b07c261c892731ce29c4880febd
E-Man Stern <92686703+not-a-ethan@users.noreply.github.com>
2024-05-05 18:49:48 -0400

    Moved files around

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ import { useForm, Controller } from 'react-hook-form';
 
 import styles from './styles.module.css'
 
-import { getProjects } from '../lib/javascript/getProjects'
-import { getTimeData } from '../lib/javascript/getTimeData'
+import { getProjects } from './getProjects'
+import { getTimeData } from './getTimeData'
 
 import  Button  from './button'
 import  ShortTextInput  from './input'
