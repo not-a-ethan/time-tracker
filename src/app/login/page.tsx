4a91aa71fe1d0c15c9a8548249bc5aef98591b92
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-08-26 19:29:25 -0400

    Fixed error on log in/out page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,29 @@
-'use client'
+"use client";
 
-import { useSession, signIn, signOut } from "next-auth/react"
-import { useRouter } from 'next/navigation'
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import styles from "./login.module.css";
 
 export default function App() {
-  const router = useRouter();
-  
-   const { data: session } = useSession()
-     if (session) {
-      router.push("/")
-       return (
-         <>
-           Signed in as {session.user?.name ?? 'unknow user'} <br />
-           <button onClick={() => signOut()}>Sign out</button>
-         </>
-       )
-     }
-     return (
-       <>
-         Not signed in <br />
-         <button onClick={() => signIn()}>Sign in</button>
-       </>
-     )
+    const router = useRouter();
+
+    const { data: session } = useSession();
+    if (session) {
+        router.push("/");
+        return (
+            <div className={styles.body}>
+                <p>Signed in as {session.user?.name ?? "unknow user"}</p>
+                <br />
+                <button onClick={() => signOut()}>Sign out</button>
+            </div>
+        );
+    }
+    return (
+        <div className={styles.body}>
+            <p>Not signed in</p>
+            <br />
+            <button onClick={() => signIn()}>Sign in</button>
+        </div>
+    );
 }
